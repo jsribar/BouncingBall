0538f7582f9059527217d2d297f20dbb38acf692
Julijan Šribar <julijan.sribar@gmail.com>
2016-11-09 21:07:22 +0100

    Line and LineIntersection classes added, tests still failling.

---
+++ BouncingBall/LineIntersections.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vsite.Pood.BouncingBall
+{
+    class LineIntersections
+    {
+        public LineIntersections(Line main)
+        {
+            this.main = main;
+        }
+
+        public PointD GetIntersection(Line l)
+        {
+            double det = main.A * l.B - l.A * main.B;
+            if (det == 0)
+                return null;
+            double x = (l.B * main.C - main.B * l.C) / det;
+            double y = (main.A * l.C - l.A * main.C) / det;
+            return new PointD(x, y);
+        }
+
+        private Line main;
+    }
+}
