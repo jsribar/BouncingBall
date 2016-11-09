a5790d541e6885daec340467dbc324cc3e3e4dd2
Bruno Noršić <brnorsic@vsite.hr>
2016-11-09 20:22:24 +0100

    PointD, Velocity and Trajectory classes defined, tests created

---
+++ BouncingBall/Velocity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Vsite.Pood.BouncingBall
+{
+    class Velocity
+    {
+        public Velocity(double speed, double angle)
+        {
+            this.speed = speed;
+            this.angle = angle;
+        }
+
+        public PointD GetNewPosition(PointD starting, double seconds)
+        {
+            double lenght = speed * seconds;
+            double deltaX = lenght * Math.Cos(angle);
+            double deltaY = lenght * Math.Sin(angle);
+            return new PointD(starting.X + deltaX, starting.Y + deltaY);
+        }
+
+        private double speed;
+        private double angle;
+    }
+}
