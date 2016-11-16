163aa59ea9fb306c8c7dd01b8e0d5113e2242f2a
Bruno Noršić <brnorsic@vsite.hr>
2016-11-16 19:42:23 +0100

    fixed errors

--- BouncingBall/Line.cs
+++ BouncingBall/Line.cs
@@ -9,8 +9,8 @@ namespace Vsite.Pood.BouncingBall
         public Line(PointD p1, PointD p2)
         {
             A = p2.Y - p1.Y;
-            B = p2.X - p1.X;
-            C = A * p1.X + B * p1.Y;
+            B = p1.X - p2.X;
+            C = -A * p1.X - B * p1.Y;
         }
 
         public readonly double A;
--- Tests/LineTests.cs
+++ Tests/LineTests.cs
@@ -14,10 +14,11 @@ namespace Vsite.Pood.BouncingBallTests
             PointD p2 = new PointD(9, 3);
             Line line = new Line(p1, p2);
             Assert.AreEqual(0, line.A, 1e-5);
-            Assert.AreEqual(4, line.B, 1e-5);
+            Assert.AreEqual(-4, line.B, 1e-5);
             Assert.AreEqual(12, line.C, 1e-5);
         }
 
+        [TestMethod]
         public void Line_CoefficientsForVerticalLine()
         {
             PointD p1 = new PointD(3, 5);
@@ -25,17 +26,29 @@ namespace Vsite.Pood.BouncingBallTests
             Line line = new Line(p1, p2);
             Assert.AreEqual(4, line.A, 1e-5);
             Assert.AreEqual(0, line.B, 1e-5);
-            Assert.AreEqual(12, line.C, 1e-5);
+            Assert.AreEqual(-12, line.C, 1e-5);
         }
 
+        [TestMethod]
         public void Line_CoefficientsForInclinedLineAt45Deg()
         {
             PointD p1 = new PointD(3, 3);
             PointD p2 = new PointD(5, 5);
             Line line = new Line(p1, p2);
             Assert.AreEqual(2, line.A, 1e-5);
-            Assert.AreEqual(2, line.B, 1e-5);
-            Assert.AreEqual(12, line.C, 1e-5);
+            Assert.AreEqual(-2, line.B, 1e-5);
+            Assert.AreEqual(0, line.C, 1e-5);
+        }
+
+        [TestMethod]
+        public void Line_CoefficientsForInclinedLineAt45DegVerticallyOffset()
+        {
+            PointD p1 = new PointD(3, 5);
+            PointD p2 = new PointD(5, 7);
+            Line line = new Line(p1, p2);
+            Assert.AreEqual(2, line.A, 1e-5);
+            Assert.AreEqual(-2, line.B, 1e-5);
+            Assert.AreEqual(4, line.C, 1e-5);
         }
     }
 }
